34350c78e2fda85bcf422a027c9db8961c2d39ab
Aleksander Thornes Vestlund <aleksander.vestlund@hotmail.com>
2024-12-19 01:11:17 +0100

    chore: Only scan wishes once

--- source/stable_matching.py
+++ source/stable_matching.py
@@ -4,16 +4,17 @@ from collections.abc import Container, Iterable, Mapping
 from matching.games import StableRoommates
 
 from source.constants import MATCHING_FILE
+from source.helpers.bidict import Bidict
 
 
-def stable_roommates(wishes: Mapping[str, Iterable[str]]) -> dict[str, str]:
+def stable_roommates(wishes: Mapping[str, Iterable[str]]) -> Bidict:
     game = StableRoommates.create_from_dictionary(wishes)
     matching = game.solve()
-    return {str(k): str(v) for k, v in matching.items()}
+    return Bidict({str(k): str(v) for k, v in matching.items()})
 
 
 def score_matching(
-    initial_wishes: Mapping[str, Container[str]], matching: Mapping[str, str]
+    initial_wishes: Mapping[str, Container[str]], matching: Bidict
 ) -> int:
     return sum(
         matching[person] in wish_list
@@ -21,8 +22,13 @@ def score_matching(
     )
 
 
-def export_matching(matching: Mapping[str, str]) -> None:
+def export_matching(matching: Bidict) -> None:
     with open(MATCHING_FILE, "w", encoding="utf-8") as file:
-        json.dump(matching, file, ensure_ascii=False, indent=4)
+        json.dump(matching.d, file, ensure_ascii=False, indent=4)
 
     print(f"Wrote matching to {MATCHING_FILE!r}.")
+
+
+def import_matching() -> Bidict:
+    with open(MATCHING_FILE, encoding="utf-8") as file:
+        return Bidict(json.load(file))
